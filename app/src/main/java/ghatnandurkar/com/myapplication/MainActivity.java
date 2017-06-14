935752f73ee2120f566bbe06e20a9440999e95a1
Amol Deshmukh <amold86@gmail.com>
2017-06-14 15:41:05 +0530

    added views

--- app/src/main/java/ghatnandurkar/com/myapplication/MainActivity.java
+++ app/src/main/java/ghatnandurkar/com/myapplication/MainActivity.java
@@ -2,12 +2,38 @@ package ghatnandurkar.com.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TextView.OnClickListener {
+
+    private EditText editText;
+    private TextView txtShowName;
+    private Button btnClickme;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // find views by ID
+        editText= (EditText) findViewById(R.id.editTxt);
+        txtShowName= (TextView) findViewById(R.id.txtShowName);
+        btnClickme= (Button) findViewById(R.id.btnClickMe);
+
+        // set onlick listeners
+        btnClickme.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnClickMe:
+                // do onclick logic here
+
+                break;
+        }
     }
 }
